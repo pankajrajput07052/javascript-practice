fcb70cbdbe76a35a9ceab315aab6134c5349b958
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2021-11-06 14:04:13 -0400

    added insert sql

--- database/index.js
+++ database/index.js
@@ -1,49 +1,65 @@
+//const mysql = require("mysql2");
+import mysql from "mysql2";
+
 function getConnection() {
-      const mysql = require("mysql2");
-      const connDetail = {
-        host: "localhost",
-        user: "root",
-        password: "password",
-        database: "mysqldb",
-      };
-      const conn = mysql.createConnection(connDetail)
-      conn.connect((err, result) => {
-            if (err) {
-                  return console.error("Error:=> connecting to mysql Database");
-            }
-            console.log("connected!!!!");
-      })
-      return conn;
+  const connDetail = {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "mysqldb",
+  };
+  const conn = mysql.createConnection(connDetail);
+  conn.connect((err, result) => {
+    if (err) {
+      return console.error("Error:=> connecting to mysql Database");
+    }
+    console.log("connected!!!!");
+  });
+  return conn;
 }
 
 function getColdInfo() {
-      var coldInfo;
-      connection.query("select * from cold_info", (err, data) => {
-            if (err) {
-                  return console.error("Error in fetching data from cold_info table");
-            }
-            coldInfo = data;
-      })
-      return coldInfo;
+  var coldInfo;
+  connection.query("select * from cold_info", (err, data) => {
+    if (err) {
+      return console.error("Error in fetching data from cold_info table");
+    }
+    coldInfo = data;
+  });
+  return coldInfo;
 }
 
 function getGrp() {
-      connection.query("select * from grp", (err, data) => {
-            if (err) {
-                  return console.error("Error in fetching data from grp table");
-            }
-            return data;
-      })
+  connection.query("select * from grp", (err, data) => {
+    if (err) {
+      return console.error("Error in fetching data from grp table");
+    }
+    return data;
+  });
 }
 
-function getWholeData(tableName){
-      connection.query(`select * from ${tableName}`, (err,data) => {
-            if(err){
-                  return console.error(`Error while getting table ${tableName} `);
-            }
-            console.log(`getting data from table ${tableName}`);
-            console.log(data);
-      })
+function getWholeData(tableName) {
+  connection.query(`select * from ${tableName}`, (err, data) => {
+    if (err) {
+      return console.error(`Error while getting table ${tableName} `);
+    }
+    console.log(`getting data from table ${tableName}`);
+    console.log(data);
+  });
+}
+
+function loadColdInfo() {
+  connection.query(
+    "insert into COLD_INFO(COLD_ID,COLD_SUBMITTER,COLD_NAME,OWNER,CONTACT,ADDRESS,FACEBOOK_URL) values(3,'niniramcs','Ninnu Ram ice & cold storage pvt. ltd','Rajendra Prashad Lodhi','+91-9927083090','mainpuri road aron','https://www.facebook.com/baapucold');"
+  );
+  console.log("data inserted");
+}
+
+function loadColdInfo() {
+  connection.query(
+    "insert into COLD_INFO(COLD_ID,COLD_SUBMITTER,COLD_NAME,OWNER,CONTACT,ADDRESS,FACEBOOK_URL) values(3,'niniramcs','Ninnu Ram ice & cold storage pvt. ltd','Rajendra Prashad Lodhi','+91-9927083090','mainpuri road aron','https://www.facebook.com/baapucold');"
+  );
+  console.log("data inserted");
 }
 
 //make connection to mysql database!!
@@ -56,5 +72,7 @@ const connection = getConnection();
 //getGrp();
 
 // get data from whole tableName
-getWholeData("cold_info");
+//getWholeData("cold_info");
 //getWholeData("grp");
+
+loadColdInfo();

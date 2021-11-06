f10825b5f70a8c4fe9544b1822848b7ebcf61e70
Pankaj <pankaj.rajput07052@gmail.com>
2021-11-06 21:30:55 +0530

    commit

---
+++ database/index.js
@@ -0,0 +1,60 @@
+function getConnection() {
+      const mysql = require("mysql2");
+      const connDetail = {
+            host: "localhost",
+            user: "root",
+            password: "password",
+            database: "mysqldb"
+      }
+      const conn = mysql.createConnection(connDetail)
+      conn.connect((err, result) => {
+            if (err) {
+                  return console.error("Error:=> connecting to mysql Database");
+            }
+            console.log("connected!!!!");
+      })
+      return conn;
+}
+
+function getColdInfo() {
+      var coldInfo;
+      connection.query("select * from cold_info", (err, data) => {
+            if (err) {
+                  return console.error("Error in fetching data from cold_info table");
+            }
+            coldInfo = data;
+      })
+      return coldInfo;
+}
+
+function getGrp() {
+      connection.query("select * from grp", (err, data) => {
+            if (err) {
+                  return console.error("Error in fetching data from grp table");
+            }
+            return data;
+      })
+}
+
+function getWholeData(tableName){
+      connection.query(`select * from ${tableName}`, (err,data) => {
+            if(err){
+                  return console.error(`Error while getting table ${tableName} `);
+            }
+            console.log(`getting data from table ${tableName}`);
+            console.log(data);
+      })
+}
+
+//make connection to mysql database!!
+const connection = getConnection();
+
+// get cold_info
+//const coldInfo = getColdInfo();
+
+// get data from GRP table
+//getGrp();
+
+// get data from whole tableName
+getWholeData("cold_info");
+getWholeData("grp");
--- index.js
+++ index.js
@@ -1,60 +1,9 @@
-function getConnection() {
-      const mysql = require("mysql2");
-      const connDetail = {
-            host: "localhost",
-            user: "root",
-            password: "password",
-            database: "mysqldb"
-      }
-      const conn = mysql.createConnection(connDetail)
-      conn.connect((err, result) => {
-            if (err) {
-                  return console.error("Error:=> connecting to mysql Database");
-            }
-            console.log("connected!!!!");
-      })
-      return conn;
+const odbc = require('node-odbc');
+ 
+async function queryAccess() {
+const connection = await odbc.connect(`DSN=C:/Users/pankaj/Desktop/cold data/2021/acc.mdb`);
+const data = await connection.query('SELECT * from  amad');
+console.log(data);
 }
-
-function getColdInfo() {
-      var coldInfo;
-      connection.query("select * from cold_info", (err, data) => {
-            if (err) {
-                  return console.error("Error in fetching data from cold_info table");
-            }
-            coldInfo = data;
-      })
-      return coldInfo;
-}
-
-function getGrp() {
-      connection.query("select * from grp", (err, data) => {
-            if (err) {
-                  return console.error("Error in fetching data from grp table");
-            }
-            return data;
-      })
-}
-
-function getWholeData(tableName){
-      connection.query(`select * from ${tableName}`, (err,data) => {
-            if(err){
-                  return console.error(`Error while getting table ${tableName} `);
-            }
-            console.log(`getting data from table ${tableName}`);
-            console.log(data);
-      })
-}
-
-//make connection to mysql database!!
-const connection = getConnection();
-
-// get cold_info
-//const coldInfo = getColdInfo();
-
-// get data from GRP table
-//getGrp();
-
-// get data from whole tableName
-getWholeData("cold_info");
-getWholeData("grp");
+ 
+queryAccess();

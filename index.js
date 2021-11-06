3ac05030a8a733c074766e97d01696b125ae948a
Pankaj <pankaj.rajput07052@gmail.com>
2021-11-06 19:50:38 +0530

    refactor

---
+++ index.js
@@ -0,0 +1,48 @@
+function getConnection() {
+      const mysql = require("mysql2");
+      const connDetail = {
+            host: "localhost",
+            user: "root",
+            password: "password",
+            database: "mysqldb"
+      }
+      const conn = mysql.createConnection(connDetail)
+      conn.connect((err, result) => {
+            if (err) {
+                  return console.error("Error:=> connecting to mysql Database");
+            }
+            console.log("connected!!!!");
+      })
+      return conn;
+}
+
+function getColdInfo() {
+      var coldInfo;
+      connection.query("select * from cold_info", (err, data) => {
+            if (err) {
+                  return console.error("Error in fetching data from cold_info table");
+            }
+            coldInfo = data;
+      })
+      return coldInfo;
+}
+
+function getGrp() {
+      connection.query("select * from grp", (err, data) => {
+            if (err) {
+                  return console.error("Error in fetching data from grp table");
+            }
+            return data;
+      })
+}
+
+//make connection to mysql database!!
+const connection = getConnection();
+
+// get cold_info
+const coldInfo = getColdInfo();
+
+// get data from GRP table
+getGrp();
+
+// 
